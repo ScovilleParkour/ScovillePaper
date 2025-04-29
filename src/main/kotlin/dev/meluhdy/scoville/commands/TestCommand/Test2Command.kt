e3f06432cfef31bad7a7f143a430132f7ef2b8ce
Melody <melodydevofficial@gmail.com>
2025-04-29 04:34:23 -0400

    Initial Commit

---
+++ src/main/kotlin/dev/meluhdy/scoville/commands/TestCommand/Test2Command.kt
@@ -0,0 +1,18 @@
+package dev.meluhdy.scoville.commands.TestCommand
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.context.CommandContext
+import dev.meluhdy.melodia.command.MelodiaCommand
+import io.papermc.paper.command.brigadier.CommandSourceStack
+
+class Test2Command : MelodiaCommand("2") {
+
+    override fun getChildren(): ArrayList<MelodiaCommand> = arrayListOf()
+
+    override fun onCommand(context: CommandContext<CommandSourceStack>): Int {
+        context.source.sender.sendPlainMessage("2")
+        return Command.SINGLE_SUCCESS
+    }
+
+
+}
---
+++ src/main/kotlin/dev/meluhdy/scoville/commands/TestCommand/TestCommand.kt
@@ -0,0 +1,20 @@
+package dev.meluhdy.scoville.commands.TestCommand
+
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.context.CommandContext
+import dev.meluhdy.melodia.command.MelodiaCommand
+import io.papermc.paper.command.brigadier.CommandSourceStack
+
+class TestCommand : MelodiaCommand("test") {
+
+    override fun getChildren(): ArrayList<MelodiaCommand> = arrayListOf(
+        Test1Command(),
+        Test2Command()
+    )
+
+    override fun onCommand(context: CommandContext<CommandSourceStack>): Int {
+        context.source.sender.sendPlainMessage("FUCK!!!! TestCommand")
+        return Command.SINGLE_SUCCESS
+    }
+
+}
